8bd41cb3bcb848a845e0299c4547375770ee1f94
EvgeniyTT <tatarin.e.m@gmail.com>
2016-11-16 00:21:23 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Article from './components/article';
+
+ReactDOM.render(<Article />, document.querySelector('#container'));
---
+++ src/components/article.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Article() {
+  return (
+    <div>
+      <h1>Article title</h1>
+      <section>Article body</section>
+    </div>
+  )
+}
+
+export default Article
